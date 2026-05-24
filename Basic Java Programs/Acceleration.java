abc59b2b79bcd41ad6249bf8aff7f00b761ab967
lalithasenthil-git <llthsnthl@gmail.com>
2026-05-24 13:10:59 +0530

    Update Acceleration.java

--- Basic Java Programs/Acceleration.java
+++ Basic Java Programs/Acceleration.java
@@ -1,1 +1,18 @@
-
+package basicjavaprograms;
+import java.util.Scanner;
+public class Acceleration
+{
+	public static void main(String[] args)
+	{
+		Scanner scn =new Scanner(System.in);
+		System.out.print("Enter v1: ");
+		double v1 = scn.nextDouble();
+		System.out.print("Enter v2: ");
+		double v2 = scn.nextDouble();
+		System.out.print("Enter time");
+		double time = scn.nextDouble();
+		double avgAcc =( v1 + v2) / time;
+		System.out.println("Acceleration Time = " +avgAcc);
+		
+	}
+}
